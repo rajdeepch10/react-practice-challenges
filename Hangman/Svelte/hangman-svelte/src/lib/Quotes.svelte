c2f00d393812674e5cb9e41fb53a6cb8666971ca
rajdeepch10 <rajdeepch0@gmail.com>
2022-11-26 12:52:47 +0530

    Added Hangman projects in React & Svelte.

---
+++ Hangman/Svelte/hangman-svelte/src/lib/Quotes.svelte
@@ -0,0 +1,16 @@
+<script>
+  let quote = null;
+  const fetchQuotes = async () => {
+    const response = await fetch("https://api.quotable.io/random").then((res) =>
+      res.json()
+    );
+    quote = response.content;
+  };
+</script>
+
+<article style="border: 1px solid orange;border-radius: 2rem;padding:2rem;">
+  {quote === null ? "No Fetched Quotes!" : quote}
+</article>
+<br />
+
+<button on:click={fetchQuotes}> Fetch Quotes </button>
